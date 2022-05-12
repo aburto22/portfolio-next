527080c13e4b26db80e3e5e1543df35a6aecab15
aburto22 <aburto22@gmail.com>
2022-05-12 11:48:07 +0200

    added project card

--- components/project_card/index.tsx
+++ components/project_card/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeContext from '../../context/theme_context';
 import ICONS from '../../data/icons';
 import Image from 'next/image';
-import TechIcon from './tech_icon';
+import TechIcon from '../tech_icon';
 import useElementOnScreen from '../../hooks/use_element_on_screen';
 import { IProject } from '../../types';
 import styles from './styles.module.scss';
+import styleUtils from '../../styles/utils.module.scss'
 
 interface ProjectCardProps {
   project: IProject;
-  maxHeight: string;
-  maxWidth: string;
+  size: string;
 }
 
-const ProjectCard = ({ project, maxHeight, maxWidth }: ProjectCardProps) => {
+const ProjectCard = ({ project, size }: ProjectCardProps) => {
   const { name, img, description, page, technologies, link, github } = project;
   const [ref, isShowing] = useElementOnScreen<HTMLLIElement>();
+  const [darkTheme] = useContext(ThemeContext);
 
   const IconComponents = ICONS.filter((icon) =>
     technologies.includes(icon.icon)
@@ -26,47 +28,37 @@ const ProjectCard = ({ project, maxHeight, maxWidth }: ProjectCardProps) => {
 
   return (
     <li
-      className={`bg-white dark:bg-gray-dark-bg ${maxHeight} ${maxWidth} flex ${
-        isShowing ? "opacity-100" : "opacity-0"
-      } flex-col items-center justify-center overflow-hidden relative w-full transition-opacity duration-700 m-2 project`}
+      className={`${styles.projectCard} ${darkTheme && styles.projectCardDark} ${size} ${isShowing && styles.showing}`}
       ref={ref}
     >
-      <h2 className="opacity-0 absolute">{name}</h2>
+      <h2 className={styles.title}>{name}</h2>
       <Image
         src={`/images/projects/${img}`}
         alt={description}
-        className="max-w-full order-first border-4 border-transparent"
+        className={styles.image}
       />
-      <div className="absolute w-full h-full bg-white dark:bg-gray-dark-bg flex flex-col p-4 items-center justify-center top-full transition-all project-content">
+      <div className={`${styles.projectContent} ${darkTheme && styles.projectContentDark}`}>
         <a
           href={link}
-          className="text-xl my-2 text-center font-bold"
+          className={styles.contentTitle}
           target="_blank"
           rel="noreferrer"
         >
           {name}
         </a>
-        <p className="text-center text-xs font-bold mb-1 mt-auto">
+        <p className={styles.contentLabel}>
           Description:
         </p>
-        <p className="text-center mb-2 text-sm">{description}</p>
-        <p className="text-center text-xs font-bold mb-1 mt-2">Technologies:</p>
-        <ul className="flex flex-row justify-center items-center flex-wrap mb-2">
+        <p className={styles.contentDescription}>{description}</p>
+        <p className={styles.contentLabel}>Technologies:</p>
+        <ul className={styles.contentIconsContainer}>
           {IconComponents}
         </ul>
-        {page && (
-          <a
-            href={page}
-            className="hover:text-blue-hover underline my-4 text-blue-link dark:text-blue-link-dark dark:hover:text-blue-hover"
-          >
-            Learn more about this project
-          </a>
-        )}
-        <div className="mt-auto mb-4 flex items-center justify-around w-full">
-          <a href={link} className="button-primary w-full ml-4 mr-2">
+        <div className={styles.contentButtonContainer}>
+          <a href={link} className={`${styleUtils.buttonPrimary} ${styles.buttonPrimaryLayout}`}>
             Live
           </a>
-          <a href={github} className="button-secondary w-full ml-2 mr-4">
+          <a href={github} className={`${styleUtils.buttonSecondary} ${styles.buttonSecondaryLayout}`}>
             Repo
           </a>
         </div>
@@ -78,6 +70,5 @@ const ProjectCard = ({ project, maxHeight, maxWidth }: ProjectCardProps) => {
 export default ProjectCard;
 
 ProjectCard.defaultProps = {
-  maxHeight: "max-h-93",
-  maxWidth: " max-w-68",
+  size: styles.defaultSize,
 };
