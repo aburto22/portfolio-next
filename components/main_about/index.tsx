bed7b0e755c0a23ee772ad673b399c4d0590c675
aburto22 <aburto22@gmail.com>
2022-05-12 14:50:52 +0200

    removed unnecessary html element in main about

--- components/main_about/index.tsx
+++ components/main_about/index.tsx
@@ -53,47 +53,45 @@ const MainAbout = () => {
   return (
     <section className={styles.section}>
       <span id="about-me" className={styles.anchor} />
-      <div className={styles.container}>
+      <div className={styles.container} ref={refText}>
         <h1 className={styles.title}>About Me</h1>
-        <div className={styles.content} ref={refText}>
-          <p className={`${styles.contentText} ${isShowingText && styles.showing}`}>
-            Hi, my name is Alejandro, and I am a Full-Stack JavaScript
-            developer. I enjoy working on the back-end as much as on the
-            front-end.
-          </p>
-          <p className={`${styles.contentText} ${styles.lastContentText} ${isShowingText && styles.showing}`}>
-            I am familiar with the following technologies, and I am always
-            looking forward to learn more:
-          </p>
-          <h2 className={styles.subTitle}>Front-end</h2>
-          <ul
-            className={styles.iconContainer}
-            ref={refFront}
+        <p className={`${styles.contentText} ${isShowingText && styles.showing}`}>
+          Hi, my name is Alejandro, and I am a Full-Stack JavaScript
+          developer. I enjoy working on the back-end as much as on the
+          front-end.
+        </p>
+        <p className={`${styles.contentText} ${styles.lastContentText} ${isShowingText && styles.showing}`}>
+          I am familiar with the following technologies, and I am always
+          looking forward to learn more:
+        </p>
+        <h2 className={styles.subTitle}>Front-end</h2>
+        <ul
+          className={styles.iconContainer}
+          ref={refFront}
+        >
+          {IconsFrontEndComponents}
+        </ul>
+        <h2 className={styles.subTitle}>Back-end</h2>
+        <ul
+          className={styles.iconContainer}
+          ref={refBack}
+        >
+          {IconsBackEndComponents}
+        </ul>
+        <h2 className={styles.subTitle}>Other</h2>
+        <ul
+          className={styles.iconContainer}
+          ref={refGeneral}
+        >
+          {IconsGeneralComponents}
+        </ul>
+        <Link href={ROUTES.ABOUT_ME}>
+          <a
+            className={`${styles.link} ${darkTheme && styles.linkDark}`}
           >
-            {IconsFrontEndComponents}
-          </ul>
-          <h2 className={styles.subTitle}>Back-end</h2>
-          <ul
-            className={styles.iconContainer}
-            ref={refBack}
-          >
-            {IconsBackEndComponents}
-          </ul>
-          <h2 className={styles.subTitle}>Other</h2>
-          <ul
-            className={styles.iconContainer}
-            ref={refGeneral}
-          >
-            {IconsGeneralComponents}
-          </ul>
-          <Link href={ROUTES.ABOUT_ME}>
-            <a
-              className={`${styles.link} ${darkTheme && styles.linkDark}`}
-            >
-              Click here to learn a little bit more about me
-            </a>
-          </Link>
-        </div>
+            Click here to learn a little bit more about me
+          </a>
+        </Link>
       </div>
     </section>
   );
