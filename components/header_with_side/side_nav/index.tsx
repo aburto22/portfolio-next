eea0e6c453ecca99563e8cbc52676d92280fb38d
aburto22 <aburto22@gmail.com>
2022-05-13 08:38:34 +0200

    added side nav css

--- components/header_with_side/side_nav/index.tsx
+++ components/header_with_side/side_nav/index.tsx
@@ -1,4 +1,5 @@
-import React, { MouseEvent, SyntheticEvent, useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
+import ThemeContext from '../../../context/theme_context';
 import SideNavLink from '../side_nav_link';
 import DarkToggle from '../../dark_toggle';
 import styles from './styles.module.scss';
@@ -10,11 +11,12 @@ interface SideNavProps {
   setIsSideShowing: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function SideNav({ anchors, isSideShowing, setIsSideShowing }: SideNavProps) {
+const SideNav = ({ anchors, isSideShowing, setIsSideShowing }: SideNavProps) => {
   const navRef = useRef<HTMLElement>(null);
+  const [darkTheme] = useContext(ThemeContext);
 
   useEffect(() => {
-    const closeMenu = (event: globalThis.MouseEvent) => {
+    const closeMenu = (event: MouseEvent) => {
 
       const element = event.currentTarget as Element;
 
@@ -42,19 +44,18 @@ export default function SideNav({ anchors, isSideShowing, setIsSideShowing }: Si
 
   return (
     <aside
-      className={`w-side-nav-w p-4 fixed bg-white dark:bg-gray-dark-bg h-full border-r 
-      border-gray-200 dark:border-gray-primary z-10 ${
-        isSideShowing ? "left-0" : "-left-60"
-      } md:left-0 transition-all`}
+      className={`${styles.sideMenu} ${darkTheme && styles.sideMenuDark} ${isSideShowing && styles.sideMenuShowing}`}
       ref={navRef}
     >
-      <h2 className="text-lg border-b border-gray-200 dark:border-gray-primary text-center py-2">
+      <h2 className={`${styles.title} ${darkTheme && styles.titleDark}`}>
         Content
       </h2>
       <ul>{AnchorsComponent}</ul>
-      <div className="flex justify-center mt-4 sm:hidden">
+      <div className={styles.darkToggleContainer}>
         <DarkToggle />
       </div>
     </aside>
   );
 }
+
+export default SideNav;
