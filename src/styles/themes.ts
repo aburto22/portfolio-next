153b6c5892d183061841d6a738196a48dc558b4c
aburto22 <aburto22@gmail.com>
2022-07-20 17:25:56 +0200

    removed unused theme lines

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -22,21 +22,6 @@ export const light: DefaultTheme = {
     blueLinkHover: '#4aa6f3',
     green: 'rgb(16, 185, 129)',
     red: 'rgb(239, 68, 68)',
-
-    // lightWhite: 'white',
-    // gray200: 'rgb(229, 231, 235)',
-    // grayLight: '#fbfbfb',
-    // grayMed: '#b4b4b4',
-    // grayPrimary: '#3a3a3a',
-    // grayDarkBg: '#1a1a1a',
-    // grayDarkBgLight: '#232323',
-    // blueHover: '#4aa6f3',
-    // blueLink: '#006baa',
-    // blueLinkDark: '#5fb9ee',
-    // blueGradI: '#006baa',
-    // blueGradF: '#59a7de',
-    // green500: 'rgb(16, 185, 129)',
-    // red500: 'rgb(239, 68, 68)',
   },
   gradients: {
     blue: 'linear-gradient(to bottom right, #006baa, #59a7de)',
@@ -47,11 +32,6 @@ export const light: DefaultTheme = {
   boxShadow: {
     form: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
     button: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-  //   bsImage: 'inset 0 0 2rem 2rem rgba(256, 256, 256, 1)',
-  //   bsImageDark: 'inset 0 0 2rem 2rem rgba(26, 26, 26, 1)',
-  //   bsDark: '0 1px 3px 0 rgba(256, 256, 256, 0.1), 0 1px 2px 0 rgba(256, 256, 256, 0.06)',
-  //   bsLgDark: '0 10px 15px -3px rgba(256, 256, 256, 0.1), 0 4px 6px -2px rgba(256, 256, 256, 0.06)',
-  //   bsXlDark: '0 20px 25px -5px rgba(256, 256, 256, 0.1), 0 10px 10px -5px rgba(256, 256, 256, 0.06)',
   },
 };
 
@@ -77,21 +57,6 @@ export const dark: DefaultTheme = {
     blueLinkHover: '#4aa6f3',
     green: 'rgb(16, 185, 129)',
     red: 'rgb(239, 68, 68)',
-
-    // lightWhite: 'white',
-    // gray200: 'rgb(229, 231, 235)',
-    // grayLight: '#fbfbfb',
-    // grayMed: '#b4b4b4',
-    // grayPrimary: '#3a3a3a',
-    // grayDarkBg: '#1a1a1a',
-    // grayDarkBgLight: '#232323',
-    // blueHover: '#4aa6f3',
-    // blueLink: '#006baa',
-    // blueLinkDark: '#5fb9ee',
-    // blueGradI: '#006baa',
-    // blueGradF: '#59a7de',
-    // green500: 'rgb(16, 185, 129)',
-    // red500: 'rgb(239, 68, 68)',
   },
   gradients: {
     blue: 'linear-gradient(to bottom right, #006baa, #59a7de)',
@@ -102,10 +67,5 @@ export const dark: DefaultTheme = {
   boxShadow: {
     form: '0 1px 3px 0 rgba(256, 256, 256, 0.1), 0 1px 2px 0 rgba(256, 256, 256, 0.06)',
     button: '0 4px 6px -1px rgba(256, 256, 256, 0.1), 0 2px 4px -1px rgba(256, 256, 256, 0.06)',
-  //   bsImage: 'inset 0 0 2rem 2rem rgba(256, 256, 256, 1)',
-  //   bsImageDark: 'inset 0 0 2rem 2rem rgba(26, 26, 26, 1)',
-  //   bsMdDark: '0 4px 6px -1px rgba(256, 256, 256, 0.1), 0 2px 4px -1px rgba(256, 256, 256, 0.06)',
-  //   bsLgDark: '0 10px 15px -3px rgba(256, 256, 256, 0.1), 0 4px 6px -2px rgba(256, 256, 256, 0.06)',
-  //   bsXlDark: '0 20px 25px -5px rgba(256, 256, 256, 0.1), 0 10px 10px -5px rgba(256, 256, 256, 0.06)',
   },
 };
